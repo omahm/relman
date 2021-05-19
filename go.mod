742eebe5383cdfb4f3335b9eb77002f40d831f98
omahm <hello@onemanandhismouse.com>
2021-05-19 15:17:10 +0100

    Create a release for each repo using the incremented tag

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.16
 
 require (
 	github.com/google/go-github/v35 v35.2.0
+	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2
+	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be
 	gopkg.in/yaml.v2 v2.4.0
 )
--- main.go
+++ main.go
@@ -8,8 +8,11 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+	"syscall"
 
 	"github.com/google/go-github/v35/github"
+	"golang.org/x/crypto/ssh/terminal"
+	"golang.org/x/oauth2"
 	"gopkg.in/yaml.v2"
 )
 
@@ -44,21 +47,37 @@ func main() {
 		log.Fatalf("Unable to parse YAML file: %v", err)
 	}
 
-	githubClient := github.NewClient(nil)
+	fmt.Print("GitHub Token: ")
+	byteToken, _ := terminal.ReadPassword(int(syscall.Stdin))
+	token := string(byteToken)
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	githubClient := github.NewClient(tc)
 
 	for _, repo := range projectYaml.Repositories {
 		release, _, _ := githubClient.Repositories.GetLatestRelease(context.Background(), repoOwner, repo.Path)
-		current_release := release.GetTagName()
+		current_release_tag := release.GetTagName()
 
-		software_version := strings.Split(current_release, ".")
+		software_version := strings.Split(current_release_tag, ".")
 
 		current_minor_version, _ := strconv.Atoi(software_version[1])
 		release_minor_version := current_minor_version + 1
 		software_version[1] = strconv.Itoa(release_minor_version)
-		new_release := strings.Join(software_version, ".")
+		new_release_tag := strings.Join(software_version, ".")
 
-		fmt.Printf("Current release: %v \n", current_release)
-		fmt.Printf("New release: %v \n", new_release)
+		branch := "main"
+
+		new_release := github.RepositoryRelease{TagName: &new_release_tag, TargetCommitish: &branch}
+
+		githubClient.Repositories.CreateRelease(context.Background(), repoOwner, repo.Path, &new_release)
+
+		fmt.Printf("Current release: %v \n", current_release_tag)
+		fmt.Printf("New release: %v \n", new_release_tag)
 
 		fmt.Printf("Repo: %v @ %v (%v) \n", repo.Name, repo.Path, release.GetTagName())
 	}
